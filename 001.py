66cbf22b0b3bfdaacb47c1888c044ba720782340
beb777 <bebatbeb@gmail.com>
2021-03-10 22:02:27 +0300

    if-statement grade result display

---
+++ 001.py
@@ -0,0 +1,14 @@
+grade = float(input('enter your result '))
+if grade >= 90:
+    print(f' congratulation !!!\n      "A"')
+elif grade >= 80:
+    print("B")
+
+elif grade >= 70:
+    print("C")
+
+elif grade >= 65:
+    print("D")
+
+else:
+    print("Failing grade")
