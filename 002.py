f971b55b1a5cf52e6dc50c8283174dc573330904
beb777 <bebatbeb@gmail.com>
2021-03-10 22:34:33 +0300

    odd and even numbers

---
+++ 002.py
@@ -0,0 +1,10 @@
+print('__________________________________________________')
+# odd or even
+numbers = int(input("Enter any number: "))
+if numbers % 2 == 0:
+   print(f"Number {numbers} is Even")
+else:
+   print(f"Number {numbers} is Odd")
+
+# 
+print('___________________________________________________')
